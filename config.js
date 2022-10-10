34f810f09c944cd03728bf46e7ebc7a0cb23235e
ZerADSC <115502641+ZerADSC@users.noreply.github.com>
2022-10-10 16:21:03 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbzgKLsF_z2D9oDxcv02ZlJEmWqtWSw6Sok_Nvh9vJNhCna6_C8P8avOeBKJf2g5TQ-ztA/exec";
